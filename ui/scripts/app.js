d9a8d828244fd16f13185c60af93989730a54f87
TatyCat <williams.tatyanna@gmail.com>
2018-06-19 00:13:04 -0400

    Home Page Potiential Revision Created

--- ui/scripts/app.js
+++ ui/scripts/app.js
@@ -1,2 +1,26 @@
-let getDate = new Date();
-document.getElementById("memeDeadline").innerHTML = getDate;
+var dayAuctionEnds = "Sep 5, 2018 15:37:25";
+var countdownDate = new Date(dayAuctionEnds).getTime();
+
+// Update the count down every 1 second
+var timerCountdown = setInterval(function() {
+    var currentTime = new Date().getTime();
+    var countdown = countdownDate - currentTime;
+    
+    var daysLeft = Math.floor(countdown / (1000 * 60 * 60 * 24));
+    var hoursLeft = Math.floor((countdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutesLeft = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
+    var secondsLeft = Math.floor((countdown % (1000 * 60)) / 1000);
+    
+    document.getElementById("auctionCountdownDays").innerHTML = daysLeft;
+    document.getElementById("auctionCountdownHours").innerHTML = hoursLeft;
+    document.getElementById("auctionCountdownMinutes").innerHTML = minutesLeft;
+    document.getElementById("auctionCountdownSeconds").innerHTML = secondsLeft;
+    
+    
+    // If the count down is over, write some text 
+    if (countdown < 0) {
+        clearInterval(timerCountdown);
+    }
+}, 1000);
+
+document.getElementById("auctionCountdownEndDate").innerHTML = dayAuctionEnds;
